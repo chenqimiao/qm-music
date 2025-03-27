f719d41f90c4235982fc75b88bcf21506a0cb1e3
陈其苗 <chenqimiao1994@126.com>
2025-03-27 18:17:39 +0800

    init project

---
+++ qm-music-parent/qm-music-dao/src/main/java/com/github/chenqimiao/Main.java
@@ -0,0 +1,11 @@
+package com.github.chenqimiao;
+
+/**
+ * @author Qimiao Chen
+ * @since 2025/3/27 16:13
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}

793d57ec50615cc361f4c42c1ad0e50641020092
陈其苗 <chenqimiao1994@126.com>
2025-03-28 12:33:01 +0800

    clean useless import

--- qm-music-parent/qm-music-app/src/main/java/com/github/chenqimiao/QmMusicApplication.java
+++ qm-music-parent/qm-music-app/src/main/java/com/github/chenqimiao/QmMusicApplication.java
@@ -1,28 +1,17 @@
 package com.github.chenqimiao;
 
-import org.flywaydb.core.Flyway;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Qimiao Chen
  * @since 2025/3/27 16:09
  **/
 @SpringBootApplication(scanBasePackages = "com.github.chenqimiao")
-public class QmMusicApplication  implements CommandLineRunner {
-
-    @Autowired
-    private Flyway flyway;
+public class QmMusicApplication  {
 
     public static void main(String[] args) {
         SpringApplication.run(QmMusicApplication.class, args);
     }
 
-    @Override
-    public void run(String... args)  {
-        flyway.migrate();
-    }
 }

ce49427d4a8d32256e468ad090586960e3abf980
Qimiao Chen <chenqimiao1994@126.com>
2025-04-05 22:10:09 +0800

    redirect / to /login.html

---
+++ qm-music-parent/qm-music-app/src/main/java/com/github/chenqimiao/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.github.chenqimiao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+/**
+ * @author Qimiao Chen
+ * @since 2025/4/5 22:06
+ **/
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "redirect:/login.html";
+    }
+}

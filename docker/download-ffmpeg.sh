dd5f9890a49f0d9644c1a38a5b2b55e1f2f6b1ee
Qimiao Chen <chenqimiao1994@126.com>
2025-04-06 16:17:54 +0800

    config ci

--- docker/download-ffmpeg.sh
+++ docker/download-ffmpeg.sh
@@ -2,7 +2,22 @@
 #!/bin/bash
 set -e
 
-FFMPEG_URL="https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-amd64-static.tar.xz"
+TARGET_ARCH=${1:-amd64}
+
+case "$TARGET_ARCH" in
+    amd64)
+        PKG="amd64"
+        ;;
+    arm64)
+        PKG="arm64"
+        ;;
+    *)
+        echo "Unsupported architecture: $TARGET_ARCH"
+        exit 1
+        ;;
+esac
+
+FFMPEG_URL="https://johnvansickle.com/ffmpeg/releases/ffmpeg-release-${PKG}-static.tar.xz"
 wget -q ${FFMPEG_URL} -O ffmpeg.tar.xz
 tar xf ffmpeg.tar.xz
 mv ffmpeg-*/ffmpeg /usr/local/bin/

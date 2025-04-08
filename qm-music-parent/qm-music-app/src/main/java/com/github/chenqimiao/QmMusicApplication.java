0bf6944d6a522bd91daba701649ee19fb3268396
Qimiao Chen <chenqimiao1994@126.com>
2025-04-08 10:42:31 +0800

    optimize

--- qm-music-parent/qm-music-app/src/main/java/com/github/chenqimiao/QmMusicApplication.java
+++ qm-music-parent/qm-music-app/src/main/java/com/github/chenqimiao/QmMusicApplication.java
@@ -14,6 +14,7 @@ public class QmMusicApplication  {
 
     public static void main(String[] args) {
         System.setProperty("jsoup.unsafe.ssl", "true");
+        System.setProperty("http.keepAlive.timeout", "60");
         System.setProperty("jdk.httpclient.connectionPoolSize", "100");
         System.setProperty("jdk.httpclient.keepAliveTimeout", "60");
         SpringApplication.run(QmMusicApplication.class, args);

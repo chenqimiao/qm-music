71747494f90c3191413189a20830a9f6df310c9b
Qimiao Chen <chenqimiao1994@126.com>
2025-04-08 16:14:19 +0800

    add test for ARTIST_NAME_DELIMITER_REGX

---
+++ qm-music-parent/qm-music-app/src/test/java/com/github/chenqimiao/service/ArtistNameTest.java
@@ -0,0 +1,38 @@
+package com.github.chenqimiao.service;
+
+import com.github.chenqimiao.QmMusicApplication;
+import com.github.chenqimiao.constant.CommonConstants;
+import com.google.common.collect.Lists;
+import junit.framework.Assert;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+/**
+ * @author Qimiao Chen
+ * @since 2025/4/8 15:53
+ **/
+@SpringBootTest(classes = QmMusicApplication.class)
+@Rollback
+public class ArtistNameTest {
+
+    @Test
+    void artistNameTest() {
+        String artistNameDelimiterRegx = CommonConstants.ARTIST_NAME_DELIMITER_REGX;
+
+        List<String> originalArtistNames = Lists.newArrayList("pp and cqm"
+                , "pp & cqm", "pp , cqm", "pp 、 cqm", "pp ， cqm", "pp ； cqm"
+                , "pp \\ cqm", "pp / cqm"
+                 );
+
+        originalArtistNames.forEach(originalArtistName -> {
+            String[] split = originalArtistName.split(artistNameDelimiterRegx);
+            Assert.assertEquals(originalArtistName, split.length, 2 );
+            Assert.assertEquals(originalArtistName, split[0].trim(), "pp");
+            Assert.assertEquals(originalArtistName, split[1].trim(), "cqm" );
+
+        });
+    }
+}

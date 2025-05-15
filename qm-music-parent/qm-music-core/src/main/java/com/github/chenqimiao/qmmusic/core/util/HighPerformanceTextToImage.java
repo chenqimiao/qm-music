1ba74deb8515e10a79b7c7960432fc634db2b715
Qimiao Chen <chenqimiao1994@126.com>
2025-05-15 17:16:23 +0800

    remove font cache

--- qm-music-parent/qm-music-core/src/main/java/com/github/chenqimiao/qmmusic/core/util/HighPerformanceTextToImage.java
+++ qm-music-parent/qm-music-core/src/main/java/com/github/chenqimiao/qmmusic/core/util/HighPerformanceTextToImage.java
@@ -22,9 +22,6 @@ public abstract class HighPerformanceTextToImage {
     // 默认字体（建议使用系统支持的中文字体）
     private static final String DEFAULT_FONT_NAME = "WenQuanYi Micro Hei";
 
-    // 2. 缓存字体渲染上下文（线程安全）
-    private static final FontRenderContext FONT_RENDER_CONTEXT = new FontRenderContext(null, true, true);
-
     /**
      * 将文字渲染为 PNG 图片的字节数组
      *

64df868e28704a0431cb1c5334ebc4216268886f
Qimiao Chen <chenqimiao1994@126.com>
2026-01-04 11:39:43 +0800

    fix bug of finding of lcr files and upgrade version

--- qm-music-parent/qm-music-app/src/main/java/com/github/chenqimiao/qmmusic/app/constant/ServerConstants.java
+++ qm-music-parent/qm-music-app/src/main/java/com/github/chenqimiao/qmmusic/app/constant/ServerConstants.java
@@ -29,6 +29,6 @@ public abstract class ServerConstants {
 
     public static String OPEN_SUBSONIC_TYPE = "Qm-Music";
 
-    public static final String OPEN_SUBSONIC_SERVER_VERSION = "v2.1.1";
+    public static final String OPEN_SUBSONIC_SERVER_VERSION = "v2.1.2";
     
 }
